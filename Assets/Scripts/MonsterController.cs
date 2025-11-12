6bff71402f44823b7b9c7a57ed889b2b1e063a53
2ddhhn <2ddhhn@gmail.com>
2025-11-12 19:43:39 +0900

    MonsterController 수정
    GameManager 불러오기
    GameManager 의 currentCard 를
    불러오면, CardFunction() 실행
    -> 하려 했으나, GameManager 스크립트를 빼놓고 진행해서 컴파일 불가

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -2,20 +2,55 @@ using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
-    public float hp;
-    public float debuff_blood;
-    public float debuff_weak;
+    //몬스터의 hp는 여기서 관리.
+    // 어떤 카드가 골라지고, Player가 그 카드를 선택한 후
+    //몬스터를 클릭했을 때의 일을 여기에 구현하겠음.
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float hp;
+    public float maxhp;
+
+    private GameManager gameManager;
+
+    //스폰 수 관리
+    public static int MonsterCount;//나중에 프리팹 만들 때, MonsterCount ++ 넣어주기.
+
+
     void Start()
+    {
+        //오류 점검용
+        if(gameManager == null)
+        {
+            //카드 불러오기 전, GameManager 불러오기
+            gameManager = FindObjectOfType<GameManager>();
+        }
+
+        
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //PlayerController - 카드 선택 후 몬스터(버튼) 클릭 -> CardFunction() 실행.
+    public void CardFunction()
     {
-        
+        if(gameManager != null)
+        {
+            //Card 이름은 GameManager 에서 쓰는 이름으로 바꾸기.
+            var Card = gameManager.currentCard;
+            //무슨 카드인지 확인용
+            Debug.Log("현재 사용된 카드 : " + Card.name);
+        }
+
+        //Player가 선택한 몬스터 이름 & 카드 이름으로, 행동.
+
+        if (Card.name = "공격")
+        {
+            
+        }
+
+
     }
 
     public void Dealer()
